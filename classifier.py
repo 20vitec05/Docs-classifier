0a64580440aa4136f438099b24ff8e385d82ab70
Tatanos <vitali.karacun@gmail.com>
2024-04-20 10:36:13 +1000

    check workability

--- classifier.py
+++ classifier.py
@@ -1,10 +1,14 @@
 import numpy as np
+import os
 from model import model, class_names
 from keras.utils import img_to_array
 from pdf2image import convert_from_path
 
 model.load_weights('classifier_weights.weights.h5')
-data = convert_from_path('Workspace/pdf/example.pdf')
-for i in range(len(data)):
-    pred = model.predict(img_to_array(data[i]))
-    data[i].save(class_names[np.argmax(pred)] + str(i) +'.jpg', 'JPEG')
+
+for j in os.listdir('Workspace/pdf/'):
+    data = convert_from_path(f'Workspace/pdf/{j}')
+    for i in range(len(data)):
+        pred = model.predict(img_to_array(data[i]))
+        doc_class = class_names[np.argmax(pred)]
+        data[i].save(f'Workspace/solution/{doc_class}/{doc_class}{j}{i}.jpg', 'JPEG')
